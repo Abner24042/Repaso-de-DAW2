e42f2f2eeaffbaf4b9af6c2a3ace78a655ae8bec
Abner24042 <abner.borrego@iest.edu.mx>
2026-05-15 02:55:52 -0600

    asdaasdassqwqweq

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -16,8 +16,8 @@ interface Props {
         id: number;
         nombre: string;
         descripcion: string;
-        precio: string;
-        stock: string;
+        precio: number;
+        stock: number;
         tipo: string;
     }[]
     actualizar: (item: any) => void;
@@ -79,9 +79,9 @@ function CardList({ items, actualizar, eliminar }: Props) {
                         <h3>{item.nombre}</h3>
                         <p>{item.descripcion}</p>
                         <p>{item.precio}</p>
-                        <p style={{ color: cantStock(parseInt(item.stock)), fontWeight: 900 }}>{item.stock}</p>
+                        <p style={{ color: cantStock(item.stock), fontWeight: 900 }}>{item.stock}</p>
                         <p>{item.tipo}</p>
-                        <p>Precio del Stock: $ {preciostock(parseInt(item.stock), parseInt(item.precio))}</p>
+                        <p>{preciostock(item.stock, item.precio)}</p>
                         <button onClick={() => actualizar(item)} className="update-btn">Actualizar</button>
                         <button onClick={() => eliminar(item.id)} className="delete-btn">Eliminar</button>
                     </div>
